0403d88e0e9f1b3a03ab5eb6364106d6e830801b
Alireza Omidi <alireza530@gmail.com>
2025-01-03 00:53:43 -0800

    Update LinkedIn profile link in config.ts to reflect new username

--- app/config.ts
+++ app/config.ts
@@ -9,7 +9,7 @@ export const metaData = {
 export const socialLinks = {
   twitter: "https://twitter.com/alirezaomidi_",
   github: "https://github.com/alirezaomidi",
-  linkedin: "https://www.linkedin.com/in/alirezaomidi",
+  linkedin: "https://www.linkedin.com/in/aomidi",
   email: "mailto:aomidi@student.ubc.ca",
   instagram: "https://www.instagram.com/alireza.o.photos",
   scholar: "https://scholar.google.com/citations?user=AObjzWEAAAAJ&hl=en"
